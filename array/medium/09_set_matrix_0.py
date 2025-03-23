e482b9abf4bac95ab7a02472ef9a5bfabe412e4f
AmaN <webdevaman2@gmail.com>
2025-03-24 00:08:12 +0530

    better-

--- array/medium/09_set_matrix_0.py
+++ array/medium/09_set_matrix_0.py
@@ -1,3 +1,4 @@
+# T.C = O(n^3)
 def setZeroes(matrix: list[list[int]]) -> None:
     """
     Do not return anything, modify matrix in-place instead.
@@ -25,3 +26,22 @@ def setZeroes(matrix: list[list[int]]) -> None:
         for j in range(n):
             if matrix[i][j] == None:
                 matrix[i][j] = 0
+
+# T.C = O(n^2)
+def better(matrix: list[list[int]]):
+	m = len(matrix)
+	n = len(matrix[0])
+
+	row = [0] * m
+	col = [0] * n
+
+	for i in range(m):
+		for j in range(n):
+		        if matrix[i][j] == 0:
+		            row[i] = 1
+		            col[j] = 1
+
+	for i in range(m):
+		for j in range(n):
+		    if col[j] == 1 or row[i] == 1:
+		        matrix[i][j] = 0
