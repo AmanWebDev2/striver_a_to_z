6ff9f8a9e4438028c5f99e3c5fee561264ba20d5
AmaN <webdevaman2@gmail.com>
2025-04-12 23:51:49 +0530

    bs laga diya

--- binary_search/bs_on_answer/02_koko_banana.py
+++ binary_search/bs_on_answer/02_koko_banana.py
@@ -16,3 +16,31 @@ def brute(piles,h):
             return speed
         else:
             speed += 1       
+
+
+"""
+    Time Complexity: O(n * log(max(piles)))
+    Space Complexity: O(1)
+"""
+ 
+def binary_search(piles,h):
+    low = 1
+    high = max(piles)
+    while low <= high:
+        mid = (low + high) // 2
+        th = getTotalHours(piles,mid)
+        if th <= h:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return low
+
+def getTotalHours(piles,k):
+    th = 0
+    for pile in piles:
+        th += math.ceil(pile/k)
+    return th
+
+
+print(brute([3,6,7,11],8))
+print(binary_search([3,6,7,11],8))
