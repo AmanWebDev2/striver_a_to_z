fa418690800040e8078702fa3d4c5e4924804a99
aman <aman@supamakers-mbp.local>
2025-04-26 23:32:43 +0530

    tc sc

--- string/easy/03_largest_odd_in_a_string.py
+++ string/easy/03_largest_odd_in_a_string.py
@@ -1,4 +1,8 @@
-def largestOddNumber(self, num: str) -> str:
+"""
+T.C: O(n)
+S.C: O(1)
+"""
+def largestOddNumber(num: str) -> str:
     for i in range(len(num)-1,-1,-1):
         if int(num[i]) % 2 != 0:
             return num[:i+1]
