ca2ebff79d3af35e333aff26d6b741fa6203c68d
AmaN <webdevaman2@gmail.com>
2025-07-14 22:29:44 +0530

    3 approach

--- potd/1290. Convert Binary Number in a Linked List to Integer.py
+++ potd/1290. Convert Binary Number in a Linked List to Integer.py
@@ -3,6 +3,9 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
+# Approach 1: Reverse the Linked List and then calculate the decimal value
 class Solution:
     def rev(self, head):
         if head is None or head.next is None:
@@ -22,3 +25,26 @@ class Solution:
             temp = temp.next
             power += 1
         return num
+
+
+# Approach 2: Bit Manipulation
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        num = 0
+        temp = head
+        while temp:
+            num = num << 1 | temp.val
+            temp = temp.next
+        return num
+
+
+# Approach 3:
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        num = ""
+        temp = head
+        while temp:
+            num += str(temp.val)
+            temp = temp.next
+
+        return int(num, 2)
